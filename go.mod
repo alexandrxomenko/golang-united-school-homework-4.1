5d0faf8d0b6ff7bc15e55b605d24db4887575837
Alexandr <alexandr.xomenko@gmail.com>
2022-05-16 22:30:45 +0300

    feat:

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alexandrxomenko/golang-united-school-homework-4.1
+
+go 1.17
--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,11 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	var a = []rune(input)
+	var b []rune
+	for i := len(a) - 1; i >= 0; i-- {
+		b = append(b, a[i])
+	}
+	output = string(b)
 	return output
 }
